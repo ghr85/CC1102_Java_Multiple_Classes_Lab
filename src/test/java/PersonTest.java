d103616d187673f189522bb0394c5ae56e037fe7
ghr85 <ghrenfrew@gmail.com>
2019-03-26 14:39:56 +0000

    added getname functionality to passenger class

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class PersonTest {
+    private Person person;
+
+    @Before
+    public void before(){
+        person = new Person("Doug");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Doug", person.getName());
+    }
 }
