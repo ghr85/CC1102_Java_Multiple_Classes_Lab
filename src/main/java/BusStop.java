dad6d21606d3c3c48bf57801d5ee3b1d5f82a7d5
ghr85 <ghrenfrew@gmail.com>
2019-03-26 15:37:54 +0000

    added add passenger functionality to queue

--- src/main/java/BusStop.java
+++ src/main/java/BusStop.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class BusStop {
+    private String name;
+    private ArrayList<Person> queue;
+
+    public BusStop(String name) {
+        this.name = name;
+        this.queue = new ArrayList<>();
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public int countQueue(){
+        return this.queue.size();
+    }
+
+    public void addPassenger(Person person){
+        this.queue.add(person);
+    }
+
+
 }
--- src/test/java/BusStopTest.java
+++ src/test/java/BusStopTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class BusStopTest {
+    private Bus bus;
+    private Person person;
+    private BusStop busStop;
+
+    @Before
+    public void before() {
+        busStop = new BusStop("Paisley");
+        person = new Person("Millicent III");
+        bus = new Bus("Ballamory",6);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Paisley",busStop.getName());
+    }
+
+    @Test
+    public void startsWithEmptyQueue(){
+        assertEquals(0,busStop.countQueue());
+    }
+    @Test
+    public void canAddPassenger(){
+        busStop.addPassenger(person);
+        assertEquals(1,busStop.countQueue());
+    }
+
+
 }
