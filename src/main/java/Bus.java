21056bce9a3287ff0c7df4df4f6396fac4e4eb6f
ghr85 <ghrenfrew@gmail.com>
2019-03-26 14:51:22 +0000

    added test for bus destination to bus class

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,2 +1,19 @@
+import com.sun.javafx.collections.ArrayListenerHelper;
+
+import java.util.ArrayList;
+
 public class Bus {
+    private String destination;
+    private int capacity;
+    private ArrayList<Person> passengers;
+
+    public Bus(String destination, int capacity){
+        this.destination = destination;
+        this.capacity = capacity;
+        this.passengers = new ArrayList<>();
+    }
+
+    public String getDestination(){
+        return this.destination;
+    }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class BusTest {
+    private Bus bus;
+    private Person person;
+    @Before
+
+    public void before(){
+        bus = new Bus("Brigadoon",8);
+        person = new Person("Shug");
+    }
+    @Test
+    public void hasDestination(){
+    assertEquals("Brigadoon", bus.getDestination());
+    }
+//    @Test
+//    public void hasCapacity{
+//
+//    }
+//    @Test
+//    public void hasPassengers(){
+//
+//    }
 }
